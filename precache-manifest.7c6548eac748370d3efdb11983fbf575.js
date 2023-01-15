57d45be79ad081f7cca13cff9a9216efbd1f4dc6
Pranav Parmar <65823784+pnparmar1089@users.noreply.github.com>
2023-01-15 23:11:25 +0530

    Updates

---
+++ precache-manifest.7c6548eac748370d3efdb11983fbf575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d716202378f0dce14d5",
+    "url": "/textutils/static/css/main.c34032f6.chunk.css"
+  },
+  {
+    "revision": "7d716202378f0dce14d5",
+    "url": "/textutils/static/js/main.7d716202.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/textutils/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "e2b365b749961938759d",
+    "url": "/textutils/static/js/2.e2b365b7.chunk.js"
+  },
+  {
+    "revision": "70d7881b58d165438560",
+    "url": "/textutils/static/js/runtime~main.70d7881b.js"
+  },
+  {
+    "revision": "9f364fd9ddcd937a3d25dbaef7f267e7",
+    "url": "/textutils/index.html"
+  }
+];
